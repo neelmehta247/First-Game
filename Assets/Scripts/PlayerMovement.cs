47dae6dd1ae58a1f1e82c0ce19cadcb4f35fe960
Neel Mehta <neelmehta247@gmail.com>
2019-04-02 19:27:40 -0400

    Add score

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,9 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-
     // This Rigidbody can be assigned from the Unity window
     public Rigidbody rb;
+
     // Can be overriden by Unity from Inspector
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -28,11 +27,13 @@ public class PlayerMovement : MonoBehaviour
         // use this to get consistent force that is not dependent on FPS
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d") || Input.GetKey("right")) {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-        if (Input.GetKey("a") || Input.GetKey("left")) {
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
